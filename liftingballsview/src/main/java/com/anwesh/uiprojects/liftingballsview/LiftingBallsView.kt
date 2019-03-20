fd866d5ddcb7d84710f2119bc7c696ba689b7bec
Anwesh43 <anweshthecool0@gmail.com>
2019-03-20 09:53:40 +0530

    adding liftingballsview module

--- liftingballsview/src/main/java/com/anwesh/uiprojects/liftingballsview/LiftingBallsView.kt
+++ liftingballsview/src/main/java/com/anwesh/uiprojects/liftingballsview/LiftingBallsView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.liftingballsview
 /**
  * Created by anweshmishra on 20/03/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val balls : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
