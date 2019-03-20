a762e668295bdc6ef331e11e08b0e35f0caa96d8
Anwesh43 <anweshthecool0@gmail.com>
2019-03-20 10:42:29 +0530

    adding drawLBNode canvas extension method

--- liftingballsview/src/main/java/com/anwesh/uiprojects/liftingballsview/LiftingBallsView.kt
+++ liftingballsview/src/main/java/com/anwesh/uiprojects/liftingballsview/LiftingBallsView.kt
@@ -20,6 +20,7 @@ val sizeFactor : Float = 2.9f
 val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#283593")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Int = 3
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -27,3 +28,31 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawLBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val xGap : Float = (2 * size) / balls
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(90f * sc2)
+    for (j in 0..(balls - 1)) {
+        val scj : Float = sc1.divideScale(j, balls)
+        val scj1 : Float = scj.divideScale(0, 2)
+        val scj2 : Float = scj.divideScale(1, 2)
+        val r : Float = xGap / rFactor
+        save()
+        translate(-size + xGap * i, size - 2 * size * scj1)
+        drawLine(0f, 0f, xGap, 0f, paint)
+        drawCircle(0f, 0f, r * scj2, paint)
+        restore()
+    }
+    restore()
+}
